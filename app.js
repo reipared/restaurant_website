80257268c3eec02bf2e85d58d95b9cd63fa339bd
Reinaldo <reinaldopared@gmail.com>
2024-01-05 12:26:45 -0400

    Update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.readyState(function () {
+$(document).ready(function () {
 	// Add smooth scrolling to all links
 	$("a").on("click", function (event) {
 		// Make sure this.hash has a value before overriding default behavior
